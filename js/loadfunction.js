df4be96269c1b8516878e4c3019a84a87694823c
Graham Carkner <gcarkner@gmail.com>
2021-01-11 22:27:31 -0500

    Add files via upload

---
+++ js/loadfunction.js
@@ -0,0 +1,11 @@
+// Graham Carkner
+// Jan 2021
+
+function loadFunction() {
+    document.getElementById("screen").style.marginLeft = "0vw";
+    document.getElementById("titlebox").style.marginLeft = "20vw";
+    document.getElementById("rightbox").style.marginLeft = "65vw";
+    setTimeout(function(){
+        document.getElementById("titlebottom").style.opacity = 1;
+    }, 500);
+}
